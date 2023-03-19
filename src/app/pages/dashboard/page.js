c86b7dfc247291f2fe2111955f3a0cb3229503c3
mudasser ahmed <mudasserahmed808@gmail.com>
2023-03-19 18:58:36 +0500

    create blog color changed

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -83,7 +83,7 @@ function page() {
         <div className="mt-10">
           <button
             type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="block w-full rounded-md bg-[#1E293B] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Create 
           </button>

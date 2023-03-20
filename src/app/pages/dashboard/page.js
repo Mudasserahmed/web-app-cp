426a18dfcc775fea9f811f1bd7288edd5595e68a
mudasser ahmed <mudasserahmed808@gmail.com>
2023-03-20 14:41:16 +0500

    quill image upload functionality added ui changed

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,71 +1,111 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.snow.css';
+import "react-quill/dist/quill.snow.css";
 
-function page() {
+function Page() {
+  //initial state values
+  const [blogData, setBlogData] = useState({
+    title: "",
+    quill: "",
+  });
+  //handle data change
+  const handleChange = (flag,e)=>{
+    console.log(e)
+    if(flag == "quill"){
+      setBlogData((prev)=>({
+        ...prev,
+        quill:e
+      }))
+    }
+    else if(flag == "title"){ 
+    const {name,value} = e.target 
+    setBlogData((pre)=>({
+      ...pre,
+      [name]:value
+    }))
+  }
+  }
+  //handle data submit
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    console.log("blog dataa>>>>>",blogData)
+  }
+  //quill modules
+  const modules = {
+    toolbar: [
+      [{ header: "1" }, { header: "2" }, { font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  };
   return (
-    <main className="isolate bg-[#F1F5F9]  py-24 px-6 sm:py-3 lg:px-8">
-      {/* <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem] ">
-        <svg
-          className="relative left-1/2 -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-40rem)] sm:h-[42.375rem]"
-          viewBox="0 0 1155 678"
-        >
-          <path
-            fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
-            fill-opacity=".3"
-            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-          />
-
-        </svg>
-      </div> */}
-      <div className="bg-white mx-96 py-5">
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Create A  Blog
-        </h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
-          Create content of the new blog here 
-        </p>
-      </div>
-      <form action="#" method="POST" className="mx-auto mt-3 max-w-xl sm:mt-10 ">
-        <div className="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
-          
-          <div className="sm:col-span-2">
-            <label
-              for="company"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Title
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="company"
-                id="company"
-                autocomplete="organization"
-                className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2 mb-5">
-            <label
-              for="message"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Description
-            </label>
-            <div className="mt-2.5">
-              <ReactQuill
-              theme="snow"
-              style={{height:"200px" ,marginBottom:"30px"}}
-              />
-            </div>
-          </div>
+    <main className=" bg-[#F1F5F9]  py-24 px-6  lg:px-8">
+      <div className="bg-white py-5 mx-4 px-5 sm:mx-10 ">
+        <div className=" text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Create A Blog
+          </h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Create content of the new blog here
+          </p>
         </div>
-        <div className="sm:col-span-2">
+        <form
+          action="#"
+          method="POST"
+          className="mx-auto mt-3 max-w-xl sm:mt-10 "
+        >
+          <div className="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="company"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Title
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="title"
+                  value={blogData?.title}
+                  onChange={(e) => handleChange("title",e)}
+                  className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2 mb-5">
+              <label
+                htmlFor="message"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Description
+              </label>
+              <div className="mt-2.5 mb-14 sm:mb-5">
+                <ReactQuill
+                  theme="snow"
+                  modules={modules}
+                  value={blogData?.quill}
+                  onChange={(e) => handleChange("quill",e)}
+                  style={{ height: "200px", marginBottom: "30px" }}
+                />
+              </div>
+            </div>
+          </div>
+          {/* <div className="sm:col-span-2 mt-14 sm:mt-6">
             <label
-              for="company"
+              htmlFor="company"
               className="block text-sm font-semibold leading-6 text-gray-900"
             >
               Upload Image
@@ -79,19 +119,20 @@ function page() {
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
+          </div> */}
+          <div className="mt-10">
+            <button
+              type="submit"
+              onClick={(e)=>handleSubmit(e)}
+              className="block w-full rounded-md bg-[#1E293B] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Create
+            </button>
           </div>
-        <div className="mt-10">
-          <button
-            type="submit"
-            className="block w-full rounded-md bg-[#1E293B] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Create 
-          </button>
-        </div>
-      </form>
+        </form>
       </div>
     </main>
   );
 }
 
-export default page;
+export default Page;

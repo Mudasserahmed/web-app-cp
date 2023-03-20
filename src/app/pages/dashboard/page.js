f545f498b028a8fa37e48bc6598b53228e4d0660
mudasser ahmed <mudasserahmed808@gmail.com>
2023-03-20 15:01:05 +0500

    image upload functionality added

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -8,6 +8,7 @@ function Page() {
   const [blogData, setBlogData] = useState({
     title: "",
     quill: "",
+    file:""
   });
   //handle data change
   const handleChange = (flag,e)=>{
@@ -25,6 +26,13 @@ function Page() {
       [name]:value
     }))
   }
+  }
+  const handleFileChange = (event) =>{
+   console.log(event)
+   setBlogData((prev)=>({
+    ...prev,
+    file:event
+   }))
   }
   //handle data submit
   const handleSubmit =(e)=>{
@@ -53,7 +61,7 @@ function Page() {
   };
   return (
     <main className=" bg-[#F1F5F9]  py-24 px-6  lg:px-8">
-      <div className="bg-white py-5 mx-4 px-5 sm:mx-10 ">
+      <div className="bg-white py-5 mx-4 px-5 md:mx-40 ">
         <div className=" text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Create A Blog
@@ -103,7 +111,7 @@ function Page() {
               </div>
             </div>
           </div>
-          {/* <div className="sm:col-span-2 mt-14 sm:mt-6">
+          <div className="sm:col-span-2 mt-14 sm:mt-6">
             <label
               htmlFor="company"
               className="block text-sm font-semibold leading-6 text-gray-900"
@@ -112,14 +120,14 @@ function Page() {
             </label>
             <div className="mt-2.5">
               <input
-                name="company"
-                id="company"
+                name="file"
+                // value={blogData?.file}
+                onChange={(e)=>handleFileChange(e.target.files[0])}
                 type="file"
-                autocomplete="organization"
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
-          </div> */}
+          </div>
           <div className="mt-10">
             <button
               type="submit"

ec023234843496d277f6e603b56ee21f25d5dd64
mudasser ahmed <mudasserahmed808@gmail.com>
2023-03-20 16:39:03 +0500

    validation added

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -2,43 +2,56 @@
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Page() {
   //initial state values
   const [blogData, setBlogData] = useState({
     title: "",
     quill: "",
-    file:""
+    file: "",
   });
-  //handle data change
-  const handleChange = (flag,e)=>{
-    console.log(e)
-    if(flag == "quill"){
-      setBlogData((prev)=>({
+  const [errors , setErrors] = useState("")
+  //handle data change function
+  const handleChange = (flag, e) => {
+    console.log(e);
+    if (flag == "quill") {
+      setBlogData((prev) => ({
         ...prev,
-        quill:e
-      }))
+        quill: e,
+      }));
+    } else if (flag == "title") {
+      const { name, value } = e.target;
+      setBlogData((pre) => ({
+        ...pre,
+        [name]: value,
+      }));
     }
-    else if(flag == "title"){ 
-    const {name,value} = e.target 
-    setBlogData((pre)=>({
-      ...pre,
-      [name]:value
-    }))
-  }
-  }
-  const handleFileChange = (event) =>{
-   console.log(event)
-   setBlogData((prev)=>({
-    ...prev,
-    file:event
-   }))
-  }
-  //handle data submit
-  const handleSubmit =(e)=>{
-    e.preventDefault()
-    console.log("blog dataa>>>>>",blogData)
-  }
+  };
+  const handleFileChange = (event) => {
+    console.log(event);
+    setBlogData((prev) => ({
+      ...prev,
+      file: event,
+    }));
+  };
+  //handle data submit function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(blogData?.file == "" || blogData?.title == "" || blogData?.quill == "" ){
+      console.log("fileds empty")
+      setErrors("Required")
+      toast.error("All Fields are Required")
+      return;
+    }
+    console.log("blog data>>>>",blogData)
+    setBlogData({
+      title: "",
+      quill: "",
+      file: "",
+    });
+  };
   //quill modules
   const modules = {
     toolbar: [
@@ -88,9 +101,10 @@ function Page() {
                   type="text"
                   name="title"
                   value={blogData?.title}
-                  onChange={(e) => handleChange("title",e)}
+                  onChange={(e) => handleChange("title", e)}
                   className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                {/* <p className="text-red-500"> {errors ? errors : ""}</p> */}
               </div>
             </div>
             <div className="sm:col-span-2 mb-5">
@@ -105,11 +119,12 @@ function Page() {
                   theme="snow"
                   modules={modules}
                   value={blogData?.quill}
-                  onChange={(e) => handleChange("quill",e)}
+                  onChange={(e) => handleChange("quill", e)}
                   style={{ height: "200px", marginBottom: "30px" }}
                 />
               </div>
             </div>
+              {/* <p className="text-red-500"> {errors ? errors : ""}</p> */}
           </div>
           <div className="sm:col-span-2 mt-14 sm:mt-6">
             <label
@@ -122,22 +137,24 @@ function Page() {
               <input
                 name="file"
                 // value={blogData?.file}
-                onChange={(e)=>handleFileChange(e.target.files[0])}
+                onChange={(e) => handleFileChange(e.target.files[0])}
                 type="file"
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
+                {/* <p className="text-red-500"> {errors ? errors : ""}</p> */}
             </div>
           </div>
           <div className="mt-10">
             <button
               type="submit"
-              onClick={(e)=>handleSubmit(e)}
+              onClick={(e) => handleSubmit(e)}
               className="block w-full rounded-md bg-[#1E293B] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Create
             </button>
           </div>
         </form>
+        <ToastContainer />
       </div>
     </main>
   );

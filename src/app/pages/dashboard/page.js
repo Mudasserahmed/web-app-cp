0c67b177f55fd7194d6eec83fd537182cfb6609a
mudasser ahmed <mudasserahmed808@gmail.com>
2023-03-20 18:51:43 +0500

    ui changes

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -29,6 +29,7 @@ function Page() {
       }));
     }
   };
+  //handle file change
   const handleFileChange = (event) => {
     console.log(event);
     setBlogData((prev) => ({
@@ -73,8 +74,8 @@ function Page() {
     },
   };
   return (
-    <main className=" bg-[#F1F5F9]  py-24 px-6  lg:px-8">
-      <div className="bg-white py-5 mx-4 px-5 md:mx-40 ">
+    <main className=" bg-[#F1F5F9]  py-24 px-6  lg:px-8 flex justify-center items-center">
+      <div className="bg-white py-5 mx-4 px-5  lg:w-6/12 md:w-6/12 ">
         <div className=" text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Create A Blog
@@ -86,7 +87,7 @@ function Page() {
         <form
           action="#"
           method="POST"
-          className="mx-auto mt-3 max-w-xl sm:mt-10 "
+          className="mx-auto mt-3 max-w-[100%] sm:mt-10 "
         >
           <div className="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
             <div className="sm:col-span-2">
@@ -104,7 +105,6 @@ function Page() {
                   onChange={(e) => handleChange("title", e)}
                   className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
-                {/* <p className="text-red-500"> {errors ? errors : ""}</p> */}
               </div>
             </div>
             <div className="sm:col-span-2 mb-5">
@@ -124,7 +124,6 @@ function Page() {
                 />
               </div>
             </div>
-              {/* <p className="text-red-500"> {errors ? errors : ""}</p> */}
           </div>
           <div className="sm:col-span-2 mt-14 sm:mt-6">
             <label
@@ -136,19 +135,17 @@ function Page() {
             <div className="mt-2.5">
               <input
                 name="file"
-                // value={blogData?.file}
                 onChange={(e) => handleFileChange(e.target.files[0])}
                 type="file"
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
-                {/* <p className="text-red-500"> {errors ? errors : ""}</p> */}
             </div>
           </div>
           <div className="mt-10">
             <button
               type="submit"
               onClick={(e) => handleSubmit(e)}
-              className="block w-full rounded-md bg-[#1E293B] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="block w-full rounded-md bg-[#1E293B] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Create
             </button>

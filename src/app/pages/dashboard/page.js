9405d25badb1fbe148e975c2260ca640f69c31a9
mudasser ahmed <mudasserahmed808@gmail.com>
2023-03-28 16:19:52 +0500

    add multiple option on file upload

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -140,6 +140,7 @@ function Dashboard() {
                 // value={blogData?.file}
                 onChange={(e) => handleFileChange(e.target.files[0])}
                 type="file"
+                multiple
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
